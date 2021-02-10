3543e0d9728f4a4f022089add5617af79c703b64
Bilyana Yordanova <sarcoholic98@gmail.com>
2021-02-10 18:02:43 +0200

    Change plot type

--- code/visualize.py
+++ code/visualize.py
@@ -13,14 +13,14 @@ def plot_AB(iterations, internal_units):
         iterations, internal_units)
 
     _, p = plt.subplots()
-
+    x = np.arange(internal_units)
     for i in range(iterations):
-        plt.plot(dataA[i, :], label='Values after iteration № ' + str(i + 1))
+        plt.scatter(x, dataA[i, :],
+                    label='Values after iteration № ' + str(i + 1), s=10)
 
     p.set(xlabel='nth neuron', ylabel='value of a',
-          title='Variance of the parameter \'a\' for ' + str(iterations) + ' of IP tuning')
-    plt.xticks(np.arange(internal_units))
-    p.grid()
+          title='Variance of the parameter \'a\' for ' + str(iterations) + ' iterations of IP tuning')
+
     plt.legend(loc="upper right")
     plt.show()
 
@@ -30,12 +30,13 @@ def plot_AB(iterations, internal_units):
 
     _, p = plt.subplots()
 
+    x = np.arange(internal_units)
     for i in range(iterations):
-        plt.plot(dataB[i, :], label='Values after iteration № ' + str(i + 1))
+        plt.scatter(x, dataB[i, :],
+                 label='Values after iteration № ' + str(i + 1), s=10)
 
     p.set(xlabel='nth neuron', ylabel='value of b',
-          title='Variance of the parameter \'b\' for ' + str(iterations) + ' of IP tuning')
-    plt.xticks(np.arange(internal_units))
-    p.grid()
+          title='Variance of the parameter \'b\' for ' + str(iterations) + ' iterations of IP tuning')
+
     plt.legend(loc="upper right")
     plt.show()
